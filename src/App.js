cdb9558e1f4e6d1b8703c5de43aac1b7501d6f21
Diotr <35659983+Diotr@users.noreply.github.com>
2019-10-20 21:29:16 +0100

    collision check propbably working
    :wq

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import React, { Component } from 'react';
 
 import Layout from './Layout/Layout';
 import Board from './Board';
-let gravity = 0;
-let xPosition = 5;
-let randomPiece=
-[
-  [1, 0, 0],
-  [1, 0, 0],
-  [1, 1, 0]
-]
+import { yieldExpression } from '@babel/types';
+let yOffset = -1;
+let xOffset = 5;
+let randomPiece =
+  [
+    [0, 0, 0],
+    [0, 2, 2],
+    [2, 2, 0]
+  ]
+const tetromino = [{
+  row: 0,
+  x: 5,
+  blockColor: 1
+}
+];
+const tetr = [{ y: 0, x: 5, blockColor: 1 },
+{ y: 0, x: 5, blockColor: 1 },
+{ y: 0, x: 6, blockColor: 1 },
+{ y: 0, x: 7, blockColor: 1 },
+{ y: 1, x: 6, blockColor: 1 },
+{ y: 2, x: 5, blockColor: 1 },
+{ y: 2, x: 6, blockColor: 1 },
+{ y: 2, x: 7, blockColor: 1 }]
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
       board: [
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -48,10 +63,10 @@ class App extends Component {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 2, 2, 2, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -69,54 +84,74 @@ class App extends Component {
         ],
     }
   }
-  dropCollision = () => {
-    //console.log(y, '====', gravity, ' ', x, ' ===', xPosition, 'val', value)
-    console.log( (this.state.board.some((row, y) => row.some((value, x) => (y===gravity && x===xPosition &&value !=0,
-      console.log(y,gravity,x,xPosition,value)
-     )
-       ))))
-      return this.dropCollision}
 
+   dropCollision = () => {
+
+   randomPiece.forEach((row,yPos)=>
+   row.forEach((blockColor,xPos)=>{
+     if(blockColor!=0){
+      console.log("iff", yPos+yOffset, xPos+xOffset, blockColor);
+      this.state.gameBoard.forEach((row,y)=>row.some((value,x)=>{
+        if(xPos+xOffset<0||xPos+xOffset>9||yPos+yOffset>19||(y===yPos+yOffset&&x===xPos+xOffset&&value !==0))
+      {
+          console.log('koliduje',y,x,yPos+yOffset,xPos+xOffset);
+          return true;
+        }
+      }))
+     }
+   }))
+   return false;
+  }
+ 
   sideCollision = () => { }
 
   playerReset = () => { }
 
   lockTetromino = () => { }
+  startGame = () => {
+    this.updateTetrominoPosition();
+  }
   ////////////////////////////////
   moveRight = () => {
-    xPosition = xPosition + 1;
+    xOffset += 1;
     this.updateTetrominoPosition()
   }
 
   moveLeft = () => {
-    xPosition = xPosition - 1;
+    xOffset -= 1;
     this.updateTetrominoPosition()
   }
 
 
   tetrominoDrop = () => {
 
-     this.dropCollision() 
-     
+    //console.log('przed drop',this.state.board)
+    console.log('tetrchecj',this.dropCollision())
+    // console.log('drop upd',this.state.board)
+
     this.updateTetrominoPosition()
-    gravity = gravity + 1;
+    //console.log('po upd',this.state.board)
+    //////////////////////////////// tu coś trzeba zmienić, jest coś źle z callbackiem/////////////////
+
+
+    yOffset += 1;
   }
   //////////////////////
   updateTetrominoPosition = () => {
     this.setState({ board: this.state.gameBoard },
       () => { this.draw() })
-      console.log("update")
+    console.log("update")
   }
 
 
   /////////////////////////
-  rotateTetromino = () => {return randomPiece=this.rotate(randomPiece), this.updateTetrominoPosition() }
+  rotateTetromino = () => { return randomPiece = this.rotate(randomPiece), this.updateTetrominoPosition() }
 
   rotate = (randomPiece) => {
     console.log("rotate")
-    const rotatedPiece =randomPiece.map((_, index) => randomPiece.map(col => col[index]),
+    const rotatedPiece = randomPiece.map((_, index) => randomPiece.map(col => col[index]),
     );
-     return (rotatedPiece.map(row => row.reverse()));
+    return (rotatedPiece.map(row => row.reverse()));
     //return console.log(rotatedPiece.reverse());
 
   };
@@ -124,11 +159,11 @@ class App extends Component {
   draw = () => {
 
     let newBoard = this.state.board;
-    randomPiece.forEach((row, yPos) => {
-      row.forEach((blockColor, xPos) => {
+    randomPiece.forEach((row,yPos) => {
+    row.forEach((blockColor, xPos) => {
         if (blockColor !== 0) {
-          newBoard = newBoard.map((row, y) => row.map((value, x) => {
-            if ((y === yPos + gravity && x === xPos + xPosition && blockColor !== 0)) {
+          newBoard = newBoard.map((row,y) => row.map((value, x) => {
+            if ((y === (yPos+ yOffset) && x === xPos + xOffset && blockColor !== 0)) {
               return value = blockColor
             } else {
               return value
@@ -140,11 +175,13 @@ class App extends Component {
     this.setState({
       board: newBoard
     }, () => {
-      console.log("draw: board", this.state.board)
+      console.log("draw: gameBboard", this.state.gameBoard)
+
     });
 
   }
 
+  /////////////
   render() {
     return (
       <Layout>
@@ -152,11 +189,12 @@ class App extends Component {
           <Board board={this.state.board} />
 
         </div>
-        <button type="button" onClick={this.tetrominoDrop}>Start</button>
-
+        <button type="button" onClick={this.startGame}>Start</button>
+        <button type="button" onClick={this.tetrominoDrop}>drop</button>
         <button type="button" onClick={this.rotateTetromino}>rotate</button>
         <button type="button" onClick={this.moveLeft}>left</button>
         <button type="button" onClick={this.moveRight}>right</button>
+        <button type="button" onClick={this.dropCollision}>d</button>
       </Layout>
     )
   }
