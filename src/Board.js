d574b172ab17949ad353d59ad369e42110a525a3
Diotr <35659983+Diotr@users.noreply.github.com>
2019-10-29 19:36:34 +0000

    Board.js

---
+++ src/Board.js
@@ -0,0 +1,24 @@
+import React from 'react';
+class Board extends React.Component{
+  render(){
+  return (
+   <div>
+     {this.props.board.map((row,y)=>
+       row.map((value,x)=>{
+         
+         const style ={
+          top: (y*40) , left: (x*40),
+          backgroundColor: 'red'
+         }
+         if(value===0){style.backgroundColor = 'blue'}
+         else if (value ===1){style.backgroundColor = 'green'}
+         else if (value ===2){style.backgroundColor = 'yellow'}
+         else if (value ===3){style.backgroundColor = 'purple'}
+         //console.log("row: "+row+"x: "+x+"y: "+y);
+         return(<div className = "block" style = {style}></div>)
+       })
+     )}
+   </div>
+  );
+}}
+export default Board;
