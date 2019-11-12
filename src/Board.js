40923ffc963308e36615089385db4e6a75461805
Diotr <35659983+Diotr@users.noreply.github.com>
2019-11-12 21:05:19 +0000

    added transparent blocks

--- src/Board.js
+++ src/Board.js
@@ -7,14 +7,15 @@ class Board extends React.Component{
        row.map((value,x)=>{
          
          const style ={
-          top: (y*40) , left: (x*40),
+          top: (y*30) , left: (x*30),
          // backgroundColor: 'red',
-         background: 'linear-gradient(#e66465, #9198e5)'
+        background: 'linear-gradient(#e66465, #9198e5)'
+        
           
          }
-         if(value===0){style.background = 'linear-gradient(#c1d9e8, #a0cce8)'}
+         if(value===0){style.background = 'rgba(0,51,0,0.2)'}//'linear-gradient(#c1d9e8, #a0cce8)'}
          else if (value ===1){style.background = 'linear-gradient(#4432a8, #a83248)'}
-         else if (value ===2){style.background = 'linear-gradient(#0d174f, #a83248)'}
+         else if (value ===2){style.background = 'linear-gradient(#e6f51b, #676e04)'}
          else if (value ===3){style.background = 'linear-gradient(#064a04, #a83248)'}
          else if (value ===4){style.background = 'linear-gradient(#4a0411, #a83248)'}
          else if (value ===5){style.background = 'linear-gradient(#4432a8, #4a0411)'}
